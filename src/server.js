22577750022a319954779571189fc571711966c6
Han Min Thaw <hanminthaw15@gmail.com>
2026-04-22 15:01:43 +0700

    express server set up

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
 // main entry point where the server starts
+const app = require('./app');
+const env = require('./config/env')
+
+app.listen(env.PORT, ()=>{
+    console.log(`Server is listening on ${env.PORT}`);
+    
+})
